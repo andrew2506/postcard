013a14382944e11c797483975e9cf4f902c09f5e
andrew <andrew@andrews-iMac.local>
2014-09-19 17:49:31 -0700

    nameLabel function

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -12,13 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterAMessageTextField: UITextField!
     
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var mailButton: UIButton!
-    
     
     
     override func viewDidLoad() {
@@ -37,11 +39,17 @@ class ViewController: UIViewController {
         // Code will evaluate when we press the button
         //adding a comment here to test git
         messageLabel.hidden = false
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
         messageLabel.text = enterAMessageTextField.text
         enterAMessageTextField.text = ""
         enterAMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        nameLabel.textColor = UIColor.blueColor()
+        
         
     }
 }
